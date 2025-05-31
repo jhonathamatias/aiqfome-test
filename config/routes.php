0758dcdcccc797b6b08d31da9d9d474be3bf27f7
Jhonatha Matias <jhonathamatias35@gmail.com>
2025-05-31 15:45:03 -0300

    feat: add route for create client

--- config/routes.php
+++ config/routes.php
@@ -10,9 +10,14 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 use Hyperf\HttpServer\Router\Router;
+use App\Controller;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
 
 Router::get('/favicon.ico', function () {
     return '';
+});
+
+Router::addGroup('/api/v1', function () {
+    Router::post('/clients', [Controller\ClientController::class, 'create']);
 });
